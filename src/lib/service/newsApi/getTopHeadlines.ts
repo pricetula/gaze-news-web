680ca9af7af735c2667a85d189438e28700eb6d1
Price Tula <pricetula@gmail.com>
2025-06-13 17:40:28 +0300

    add functions to make get request to the news api, get top headlines and tested all cases

---
+++ src/lib/service/newsApi/getTopHeadlines.ts
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Provides a function to fetch the latest top headlines from the News API.
+ * It might throw NewsApiError exceptions if an issue arises.
+ */
+
+import { NewsApiError } from "./error";
+import { getNewsApiData } from "./getNewsApiData";
+
+/**
+ * Fetches the latest top headlines from the News API.
+ * This function first constructs the full API URL using the `getUrl` utility,
+ * then performs an HTTP GET request to that URL. It handles potential network
+ * errors and non-successful HTTP responses by throwing `NewsApiError` instances.
+ *
+ * @returns {Promise<Article[]>} A Promise that resolves with the parsed Article JSON data from the API response.
+ * @throws {NewsApiError} If there's a network error, a non-2xx HTTP response from the API,
+ * or issues with URL construction (propagated from `getUrl`).
+ */
+export async function getTopHeadlines(): Promise<Article[]> {
+    const response = await getNewsApiData<NewsResponse>(
+        '/top-headlines',
+        {
+            country: 'us',
+        }
+    )
+    if (response.status !== "ok") {
+        throw new NewsApiError(
+            "attempting to get top headlines failed response status is not ok",
+        );
+    }
+    return response.articles;
+}
