12d81b06cdb619d5a47de9c4b783b85646744cbe
Price Tula <pricetula@gmail.com>
2025-06-16 18:58:28 +0300

    clean up

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,7 +1,46 @@
 import { NextRequest } from "next/server";
+import * as Sentry from "@sentry/nextjs";
+import { saveArticles } from "@/db/articles/save";
+import { getEverything } from "@/lib/service/newsApi/getEverything";
+import { DBError } from "@/db/error";
+import { NewsApiError } from "@/lib/service/newsApi/error";
 
 // GET request to fetch news from the news api, sync it to our database and return results as response
 export async function GET(request: NextRequest) {
+    // Get search params from the urql
+    const { searchParams } = new URL(request.url);
+
+    // If no search params found then return error response
+    if (!searchParams) return new Response(
+        JSON.stringify({ message: "search params are missing" }),
+        { status: 400 }
+    )
+
+    // Obtain key words from the urql
+    const keyWords = searchParams.get('keywords');
+    if (!keyWords) return new Response(
+        JSON.stringify({ message: "keywords for search are missing" }),
+        { status: 400 }
+    )
+
+    // Attempt to save articles obtained from news api
+    try {
+        await saveArticles(await getEverything({ keyWords }));
+    } catch(error: any) {
+        if ([DBError, NewsApiError].includes(error.constructor)) {
+            console.log('eeeeeee-------------')
+            // Log the error to sentry
+            Sentry.captureException(error);
+            await Sentry.flush(2000);
+        }
+
+        // Throw exception
+        return new Response(
+            JSON.stringify({ message: "Failed to save articles to database" }),
+            { status: 500 }
+        )
+    }
+
     return new Response(JSON.stringify({ message: "Hello from API" }), {
         headers: { 'Content-Type': 'application/json' },
     });
--- src/db/articles/save.ts
+++ src/db/articles/save.ts
@@ -1,44 +1,91 @@
-import { Article } from "@/lib/service/newsApi/types";
-import { sources, authors } from "../schema";
+import { Article, NewSource } from "@/lib/service/newsApi/types";
+import { sources, authors, articles } from "../schema";
 import { db } from "../drizzledb";
+import { DBError } from "../error";
 
 export async function saveArticles(articlesFromNewsApi: Article[]) {
-    const sourcesToAdd = []
-    const authorsToAdd: {
+    // Define variable that will hold a collection of NewSource
+    const sourcesToAdd: Record<string, NewSource> = {}
+
+    // Define variable that will hold a collection of authors to be added
+    const authorsToAdd: Record<string, {
         id: string;
         name: string;
-    }[] = []
+    }> = {}
+
+    // Define a list that will hold articles to be added to tha database
     const articlesToAdd = []
 
+    // Iterate through each article
     for (const article of articlesFromNewsApi) {
-        if (!article.publishedAt) continue
+        // Create an Id for an author converting author name to lowercase
+        const authorId = article.author?.toLowerCase?.()?.replaceAll(' ', '-') || ''
+
+        // Create source id for article to be created
+        const sourceId = article.source.id?.toLowerCase?.()?.replaceAll(' ', '-') || ''
+
+        // If missing article published at date, author id or source id then continue to next item
+        if (!article.publishedAt || !authorId || !sourceId) continue
+
+        // Create a new source
         const source = {
-            id: article.source.id,
+            id: sourceId,
             name: article.source.name,
             description: '',
             url: '',
-            categoryId: '',
-            languageId: '',
-            countryId: ''
         }
 
+        // Create a new author
         const author = {
-            id: article.author?.toLowerCase?.()?.replaceAll(' ', '-') || '',
+            id: authorId,
             name: article.author || ''
         }
 
+        // Add articles to be added to the database
         articlesToAdd.push({
-            ...article,
+            id: crypto.randomUUID(),
             sourceId: source.id,
             authorId: author.id,
-            id: crypto.randomUUID()
+            title: article.title,
+            description: article.description,
+            url: article.url,
+            urlToImage: article.urlToImage,
+            publishedAt: new Date(article.publishedAt),
+            content: article.content,
         })
 
-        sourcesToAdd.push(source)
+        // Map the source to sourcesToAdd collection
+        if (source.id && !sourcesToAdd[source.id]) {
+            sourcesToAdd[source.id] = source
+        }
 
-        authorsToAdd.push(author)
+        // Map the author to authorsToAdd collection
+        if (author.id && !authorsToAdd[author.id]) {
+            authorsToAdd[author.id] = author
+        }
     }
 
-    // await db.insert(sources).values(sourcesToAdd)
-    await db.insert(authors).values(authorsToAdd)
+    try {
+        await db.insert(sources).values(Object.values(sourcesToAdd))
+    } catch (error: any) {
+        if (error.code !== '23505') {
+            throw new DBError(error.message)
+        }
+    }
+
+    try {
+        await db.insert(authors).values(Object.values(authorsToAdd))
+    } catch (error: any) {
+        if (error.code !== '23505') {
+            throw new DBError(error.message)
+        }
+    }
+
+    try {
+        await db.insert(articles).values(articlesToAdd)
+    } catch (error: any) {
+        if (error.code !== '23505') {
+            throw new DBError(error.message)
+        }
+    }
 }
