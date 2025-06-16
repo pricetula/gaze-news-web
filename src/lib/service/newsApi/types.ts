866521d3989735c84afe729aab54e97ef7e54996
Price Tula <pricetula@gmail.com>
2025-06-16 14:26:08 +0300

    clean up

--- src/lib/service/newsApi/types.ts
+++ src/lib/service/newsApi/types.ts
@@ -1,12 +1,12 @@
 export interface Article {
-    source: { id: string | null; name: string };
-    author: string | null;
+    source: { id: string; name: string };
+    author: string;
     title: string;
-    description: string | null;
+    description: string;
     url: string;
-    urlToImage: string | null;
+    urlToImage: string;
     publishedAt: string;
-    content: string | null;
+    content: string;
 }
 
 export interface NewsResponse {
@@ -20,7 +20,4 @@ export interface NewSource {
     name: string;
     description: string;
     url: string;
-    category: string;
-    language: string;
-    language: string;
 }
