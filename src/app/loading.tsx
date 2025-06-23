8260a4a8860429ebde8fccb4138f86c3d68aa0a3
Price Tula <pricetula@gmail.com>
2025-06-23 03:35:29 +0300

    moved tanstack query implementation to its own route

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[var(--font-roboto-sans)]">
-            loading main page...
+            loading main page...from loading component
         </div>
     );
 }
